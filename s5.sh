c399f08170e5555a52147ac8dfc1a0e5e9e585c4
kkshitij17 <125120391+kkshitij17@users.noreply.github.com>
2023-05-11 17:28:32 +0530

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -1,10 +1,14 @@
 #!home/kshitij
-#code for displaying temperature of user inputted city
-# enter city
-echo enter the city
-read  city
-# curl to get data 
-temperature=$(curl -s   http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid="05a0f76a7accfaae91413edc6710db9a")
-# Print the temperature
-echo  $temperature 
 
+# enter  the city
+echo enter city
+read city
+
+#variable to store the response from the url
+var=$(curl -s -G "http://api.openweathermap.org/data/2.5/weather" --data-urlencode "q=$city" --data-urlencode "appid=4926e6e43ef7996d15c402146dfc447c")
+
+# Extract the temperature value from the response using awk(using temp as delimiter)
+temperature=$(echo "$var" | awk -F'"temp":' '{print $2}' | awk -F',' '{print $1}')
+
+# Print  temperature
+echo "$temperature ° K."
