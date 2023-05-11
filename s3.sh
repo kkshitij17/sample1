bbd5414aa75c529cdb81573e27c3ec465ab708d5
kkshitij17 <125120391+kkshitij17@users.noreply.github.com>
2023-05-11 16:38:11 +0530

    Update s3.sh

--- s3.sh
+++ s3.sh
@@ -2,12 +2,12 @@
 #print the list of all text files
 ls *.txt
 
-#Iterate the loop to read all text files
+#reading the .txt files in CWD
 for file in `ls *.txt`;
 do
     #Read the basename of the file
     filename=`basename $file .txt`
-    #Rename all files to jpg files
+    #Rename to jpg files
     mv $file $filename.jpg;
 done
 
