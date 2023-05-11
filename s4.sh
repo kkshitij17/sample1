d270413f0d211659c5269189c2353cba4bd3a6f1
kkshitij17 <125120391+kkshitij17@users.noreply.github.com>
2023-05-11 16:40:12 +0530

    Update s4.sh

--- s4.sh
+++ s4.sh
@@ -4,12 +4,15 @@ echo enter n
 read $var
 va=0;
 i=1;
-for (( i=1 ; i<=5 ; i++)); 
+for (( i=1 ; i<=$var ; i++)); 
 do
 for (( j=1 ; j<=i ; j++ ));
 do
+#printing the element at xth pos
 echo -n  $((va%10));
+#incrementing the number
 va=$((va+1));
 done
+#change of line
 echo 
 done
