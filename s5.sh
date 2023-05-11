1b54c33328017821d2975c6405c18c6baa748a0b
kkshitij17 <125120391+kkshitij17@users.noreply.github.com>
2023-05-11 16:58:07 +0530

    Update s5.sh

--- s5.sh
+++ s5.sh
@@ -2,7 +2,7 @@
 #code for displaying temperature of user inputted city
 # enter city
 echo enter the city
-read  $city
+read  city
 # curl to get data 
 temperature=$(curl -s   http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid="05a0f76a7accfaae91413edc6710db9a")
 # Print the temperature
